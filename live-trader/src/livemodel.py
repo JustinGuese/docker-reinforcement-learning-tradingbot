3e8eb9ab6de54cbcf6efe280f89f7c38e9283abe
Justin Guese <guese.justin@gmail.com>
2021-09-16 14:09:50 +0200

    adapted to new accounthandler

--- live-trader/src/livemodel.py
+++ live-trader/src/livemodel.py
@@ -5,7 +5,7 @@ from ta import add_all_ta_features
 from os import environ
 import yfinance as yf
 import numpy as np
-import requests
+from requests import post
 import json
 
 MODEL_FILENAME = "persistent/a2cmlp.hf5"
@@ -17,6 +17,8 @@ PW = "1234"
 
 # get current data
 df = yf.download(STOCK, period="2d",interval="1h")
+# only take the last full hour
+df = df.iloc[:-1]
 print(df.tail())
 # df = df.reset_index()
 df = add_all_ta_features(df, open="Open", high="High", low="Low", close="Close", volume="Volume", fillna=True)
@@ -60,7 +62,7 @@ action, _states = model.predict(obs)
 obs, rewards, dones, info = env.step(action)
 preds.append(action[0])
 
-action = np.median(preds)
+action = action[0] # np.median(preds)
 
 actiondict = {
     0 : "Sell",
@@ -72,48 +74,50 @@ print("I PREDICT: %s" % actiondict[action], preds)
 
 ## functionality to communicate with fake backend
 if action != 0.5:
-    with requests.Session() as s:
-            payload={'email': USER,
-                    'password': PW}
-            p = s.post('%s/login' % environ["ACCOUNT_URL"], data=payload)
-            def buy(amount):
-                # first get current holdings
-                payload={'stock': STOCK,
-                    'amount': amount}
-                print("i am going to buy %d %s, because the current price is %2.f USD" % (amount, STOCK, crntPrice))
-                p = s.post('%s/api/buyStock' % environ["ACCOUNT_URL"], json=payload)
-                print(p.text)
-                if json.loads(p.text)["code"] != 200:
-                    print("something went wrong :(")
-                
-            def sell(owned):
-                print("tryna sell %d %s" % (owned, STOCK))
-                payload={'stock': STOCK}
-                p = s.post('%s/api/sellStock' % environ["ACCOUNT_URL"], json=payload)
-                print(p.text)
-                if json.loads(p.text)["code"] != 200:
-                    print("something went wrong :(")
-            # should be logged in now
-            # get current available eur
-            resp = s.get('%s/api/getPortfolio' % environ["ACCOUNT_URL"]).json()
-            euravailable = float(resp["EUR"]["amount"])
-            stockAlreadyOwned = 0
-            if resp.get(STOCK):
-                stockAlreadyOwned = float(resp[STOCK]["amount"])
-            print("i have %.2f euro to spend" % euravailable)
-            
-            crntPrice = df.iloc[-1]["Close"]
-            amount = int(euravailable / crntPrice)
-            
-            if action == 1:
-                if amount == 0:
-                    print(" cant buy bc im a poor fuck")
-                else:
-                    buy(amount)
-            elif action == 0:
-                if stockAlreadyOwned == 0:
-                    print("i don't own any stock")
-                else:
-                    sell(stockAlreadyOwned)
+    def buy(amount):
+        # first get current holdings
+        payload={
+            'user' : "rltrader",
+            'stock': STOCK,
+            'amount': amount}
+        print("i am going to buy %.2f %s, because the current price is %2.f USD" % (amount, STOCK, crntPrice))
+        p = post('%s/buy' % environ["ACCOUNT_URL"], json=payload)
+        if json.loads(p.text)["code"] != 200:
+            print("something went wrong :(: %s " % str(repr(p.text)))
+        
+    def sell(owned):
+        print("tryna sell %d %s" % (owned, STOCK))
+        payload={
+                'user' : "rltrader",
+                'stock': STOCK
+                }
+        p = post('%s/sell' % environ["ACCOUNT_URL"], json=payload)
+        if json.loads(p.text)["code"] != 200:
+            print("something went wrong :( %s " % str(repr(p.text)))
+    # should be logged in now
+    # get current available eur
+    js = {
+        "user": "rltrader",
+        }
+    resp = post('%s/getPortfolio' % environ["ACCOUNT_URL"], json=js).json()
+    euravailable = float(resp["portfolio"]["USD"])
+    stockAlreadyOwned = 0
+    if resp["portfolio"].get(STOCK):
+        stockAlreadyOwned = float(resp["portfolio"][STOCK])
+    print("i have %.2f euro to spend" % euravailable)
+    
+    crntPrice = df.iloc[-1]["Close"]
+    amount = float((euravailable*.98) / crntPrice) # accept float for crypto
+    
+    if action == 1:
+        if amount == 0:
+            print(" cant buy bc im a poor fuck")
+        else:
+            buy(amount)
+    elif action == 0:
+        if stockAlreadyOwned == 0:
+            print("i don't own any stock")
+        else:
+            sell(stockAlreadyOwned)
             
             
